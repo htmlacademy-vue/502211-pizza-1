e30ef2fe4bbd995e5179f91ff42a06b5afa0aa05
Poly Titova <poly_titova@mail.ru>
2021-10-31 20:34:40 +0300

    Подключили статические json-данные

---
+++ src/frontend/src/views/Index.vue
@@ -0,0 +1,23 @@
+<template></template>
+
+<script>
+// импортируем JSON данные
+import misc from "@/static/misc.json";
+import pizza from "@/static/pizza.json";
+import user from "@/static/user.json";
+
+export default {
+  name: "IndexHome",
+  // подключаем их в опции data компонента
+  data() {
+    return {
+      misc,
+      pizza,
+      user,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
