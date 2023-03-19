14a690aa4e2585c507b97ce259d1f6cfeae55ac5
Poly A. Titova <poly_titova@mail.ru>
2023-03-19 22:20:14 +0300

    Добавили модуль с заказами

--- src/frontend/src/common/mixins/index.js
+++ src/frontend/src/common/mixins/index.js
@@ -1,5 +1,6 @@
 import itemsCounter from "@/common/mixins/itemsCounter";
 import imageLink from "@/common/mixins/imageLink";
 import formInputClassSize from "@/common/mixins/formInputClassSize";
+import imageWithExtensionLink from "@/common/mixins/imageWithExtensionLink";
 
-export { itemsCounter, imageLink, formInputClassSize };
+export { itemsCounter, imageLink, formInputClassSize, imageWithExtensionLink };
