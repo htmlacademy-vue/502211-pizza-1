95f47effb15ba0dc5cca12b769b23e99e9eeb75f
Poly A. Titova <poly_titova@mail.ru>
2023-05-28 14:42:21 +0300

    Создали API и JWT сервисы

---
+++ src/frontend/src/services/api.service.js
@@ -0,0 +1,81 @@
+import JwtService from "@/services/jwt.service";
+import axios from "@/plugins/axios";
+
+export class ReadOnlyApiService {
+  // resource — приватное свойство класса. Добавляем его к базовому URL, чтобы получить
+  // финальный URL, на который нужно отправлять запросы
+  #resource;
+  constructor(resource) {
+    this.#resource = resource;
+  }
+
+  // запрос на получение списка сущностей
+  async query(config = {}) {
+    const { data } = await axios.get(this.#resource, config);
+    return data;
+  }
+
+  // запрос на получение одной сущности по id
+  async get(id, config = {}) {
+    const { data } = await axios.get(`${this.#resource}/${id}`, config);
+    return data;
+  }
+}
+
+// Наследуемся от Read-only API-сервиса и добавляем операции post, put, delete
+export class CrudApiService extends ReadOnlyApiService {
+  #resource;
+  constructor(resource) {
+    super(resource);
+    this.#resource = resource;
+  }
+
+  // запрос на создание сущности
+  async post(entity) {
+    const { data } = await axios.post(this.#resource, entity);
+    return data;
+  }
+
+  // запрос на обновление сущности
+  async put(entity) {
+    const { data } = await axios.put(`${this.#resource}/${entity.id}`, entity);
+    return data;
+  }
+
+  // запрос на удаление сущности
+  async delete(id) {
+    const { data } = await axios.delete(`${this.#resource}/${id}`);
+    return data;
+  }
+}
+
+// класс не подразумевает CRUD операции
+export class AuthApiService {
+  // задаём токен авторизации
+  setAuthHeader() {
+    // получаем токен из LocalStorage с помощью JWT-сервиса
+    const token = JwtService.getToken();
+    // добавляем заголовок авторизации в axios как Bearer token
+    axios.defaults.headers.common["Authorization"] = token
+      ? `Bearer ${token}`
+      : "";
+  }
+
+  async login(params) {
+    // отправляем логин/пароль для авторизации на сервере
+    const { data } = await axios.post("login", params);
+    return data;
+  }
+
+  async logout() {
+    // делаем логаут на сервере
+    const { data } = await axios.delete("logout");
+    return data;
+  }
+
+  async getMe() {
+    // получаем профиль залогиненного пользователя
+    const { data } = await axios.get("whoAmI");
+    return data;
+  }
+}
