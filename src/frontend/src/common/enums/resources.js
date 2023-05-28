05da8044b0f4dad46c97635620b261be9820cd40
Poly A. Titova <poly_titova@mail.ru>
2023-05-28 17:19:46 +0300

    Подключили сервисы плагинами в Vue и Vuex

---
+++ src/frontend/src/common/enums/resources.js
@@ -0,0 +1,11 @@
+export default {
+  AUTH: "auth",
+  USERS: "users",
+  DOUGH: "dough",
+  SIZE: "sizes",
+  SAUCE: "sauces",
+  INGREDIENTS: "ingredients",
+  MISC: "misc",
+  ADDRESSES: "addresses",
+  ORDERS: "orders",
+};
--- src/frontend/src/common/utils.js
+++ src/frontend/src/common/utils.js
@@ -1,3 +1,10 @@
+import resources from "@/common/enums/resources";
+import {
+  AuthApiService,
+  CrudApiService,
+  ReadOnlyApiService,
+} from "@/services/api.service";
+
 export const getCountSum = (items) => {
   return items.reduce((prev, curr) => prev + curr.price * curr.amount, 0);
 };
@@ -18,3 +25,17 @@ export const getIngredientsText = (ingredients) => {
 
 export const getRandomNumber = (max, min) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
+
+export const createResources = () => {
+  return {
+    [resources.AUTH]: new AuthApiService(),
+    [resources.USERS]: new ReadOnlyApiService(resources.USERS),
+    [resources.DOUGH]: new ReadOnlyApiService(resources.DOUGH),
+    [resources.SIZE]: new ReadOnlyApiService(resources.SIZE),
+    [resources.SAUCE]: new ReadOnlyApiService(resources.SAUCE),
+    [resources.INGREDIENTS]: new ReadOnlyApiService(resources.INGREDIENTS),
+    [resources.MISC]: new ReadOnlyApiService(resources.MISC),
+    [resources.ADDRESSES]: new CrudApiService(resources.ADDRESSES),
+    [resources.ORDERS]: new CrudApiService(resources.ORDERS),
+  };
+};
